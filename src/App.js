c52b122d397d5c2da8a5367fbf58aa34ceb78cb0
Devashish Bahri <devashishbahri@Devashishs-MacBook-Pro.local>
2023-03-22 20:49:12 +0530

    fill in the blanks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';  // make sure the right modules have been imported
 
-function App() {
+const App = () => {
+  const firstMessage = 'Hi! I am XXXXXX'; // change to reflect your name
+
+  const [message, setMessage] = useState();
+  const [choice, setChoice] = useState(1);  // start with : Who am I?
+
+  // insert your useEffect hook(s) below this line
+  useEffect(() => {
+    // code here
+  }, []);
+
+  const onChangeHandler = () => {
+    // code here
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to ReactJS course. Switch branch to desired topic.
-        </p>
-      </header>
+    <div style={{ padding: '1rem' }}>
+      <h1>{message}</h1>
+
+      <label>Get to know more about me: </label>
+      <select name="msg" id="msg-select" onChange={onChangeHandler}>
+        <option value="1">Who am I?</option>
+        <option value="2">Where am I?</option>
+        <option value="3">What do I know?</option>
+        <option value="4">What is my pet's name?</option>
+      </select>
     </div>
   );
 }
